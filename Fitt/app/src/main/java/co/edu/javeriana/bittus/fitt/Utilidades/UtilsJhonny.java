c895059b11225127725f51108f6bd177faef9931
Jhonny <jhonny_parra@javeriana.edu.co>
2019-05-01 01:19:54 -0500

    Se analiza parte del recorrido

---
+++ Fitt/app/src/main/java/co/edu/javeriana/bittus/fitt/Utilidades/UtilsJhonny.java
@@ -0,0 +1,17 @@
+package co.edu.javeriana.bittus.fitt.Utilidades;
+
+public class UtilsJhonny {
+
+    public static final int RADIUS_OF_EARTH_KM = 6371;
+
+    public static double distance(double lat1, double long1, double lat2, double long2) {
+        double latDistance = Math.toRadians(lat1 - lat2);
+        double lngDistance = Math.toRadians(long1 - long2);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lngDistance / 2) * Math.sin(lngDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double result = RADIUS_OF_EARTH_KM * c;
+        return Math.round(result * 100.0) / 100.0;
+    }
+}
--- Fitt/app/src/main/java/co/edu/javeriana/bittus/fitt/Vista/IniciarRecorridoActivity.java
+++ Fitt/app/src/main/java/co/edu/javeriana/bittus/fitt/Vista/IniciarRecorridoActivity.java
@@ -1,13 +1,20 @@
 package co.edu.javeriana.bittus.fitt.Vista;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.View;
+import android.widget.Chronometer;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -21,9 +28,11 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import co.edu.javeriana.bittus.fitt.R;
 import co.edu.javeriana.bittus.fitt.Utilidades.Permisos;
+import co.edu.javeriana.bittus.fitt.Utilidades.UtilsJhonny;
 
 public class IniciarRecorridoActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -33,36 +42,90 @@ public class IniciarRecorridoActivity extends FragmentActivity implements OnMapR
     private LocationCallback mLocationCallback;
     private FusedLocationProviderClient mFusedLocationClient;
 
+
+    private ImageButton iniciarRecorrido;
+    private ImageButton finalizarRecorrido;
+
+    private TextView distanciaRecorrido;
+    private double distanciaRecorridoN = 0;
+    private TextView pasosRecorridos;
+    private int pasosRecorridosN;
+    private TextView velocidadPromedioRecorrido;
+    private double velocidadPromedioRecorridoN;
+    private TextView caloriasRecorrido;
+    private double caloriasRecorridoN;
+    private TextView tiempoRecorrido;
+
+    private Chronometer chrono;
+
+
+
+
+    private double latitudUltimaUbicacion;
+    private double longitudUltimaUbicacion;
+
+    private boolean inicioColocado = false;
+
     private static final int ID_PERMISSION_LOCATION = Permisos.getIdPermissionLocation();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_prueba_mapas);
+        setContentView(R.layout.activity_iniciar_recorrido);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         mLocationRequest = createLocationRequest();
 
-        Permisos.requestPermission(this, Manifest.permission.ACCESS_FINE_LOCATION, "El permiso es necesario para acceder a la localización.", ID_PERMISSION_LOCATION);
+        distanciaRecorrido = findViewById(R.id.distanciaRecorrido);
+        pasosRecorridos = findViewById(R.id.pasosRecorridos);
+        velocidadPromedioRecorrido = findViewById(R.id.velocidadPromedioRecorrido);
+        caloriasRecorrido = findViewById(R.id.caloriasRecorrido);
+        tiempoRecorrido = findViewById(R.id.tiempoRecorrido);
 
 
+
+
+        //Lo que se hace cada 20 segs que se recibe una nueva ubicación
         mLocationCallback = new LocationCallback() {
             @Override
             public void onLocationResult(LocationResult locationResult) {
                 Location location = locationResult.getLastLocation();
 
                 if (location != null) {
+                    if (!inicioColocado){
+                        colocarMarcadorInicio (location);
+                        inicioColocado = true;
+                    }
+                    actualizarUbicacion (location);
                     Toast.makeText(getApplicationContext(),String.valueOf(location.getLatitude()) , Toast.LENGTH_LONG).show();
                     Toast.makeText(getApplicationContext(),String.valueOf(location.getLatitude()) , Toast.LENGTH_LONG).show();
                 }
             }
         };
+        inicializarCronometro();
+        mapFragment.getMapAsync(this);
 
-        startLocationUpdates();
+    }
+
+    private void inicializarCronometro(){
+        chrono  = (Chronometer) findViewById(R.id.tiempoRecorrido);
+        chrono.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener(){
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+                long time = SystemClock.elapsedRealtime() - chronometer.getBase();
+                int h   = (int)(time /3600000);
+                int m = (int)(time - h*3600000)/60000;
+                int s= (int)(time - h*3600000- m*60000)/1000 ;
+                String t = (h < 10 ? "0"+h: h)+":"+(m < 10 ? "0"+m: m)+":"+ (s < 10 ? "0"+s: s);
+                chronometer.setText(t);
+            }
+        });
+        chrono.setBase(SystemClock.elapsedRealtime());
+        chrono.setText("00:00:00");
     }
 
 
@@ -80,9 +143,23 @@ public class IniciarRecorridoActivity extends FragmentActivity implements OnMapR
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+
+        //Los botones adquieren funcionalidad únicamente cuando el mapa está listo:
+
+        iniciarRecorrido = (ImageButton) findViewById(R.id.btnIniciarRecorrido);
+        finalizarRecorrido = (ImageButton) findViewById(R.id.btnFinalizarRecorrido);
+
+        iniciarRecorrido.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Permisos.requestPermission((Activity) v.getContext(), Manifest.permission.ACCESS_FINE_LOCATION, "El permiso es necesario para acceder a la localización.", ID_PERMISSION_LOCATION);
+                startLocationUpdates();
+                chrono.start();
+            }
+        });
+
+
     }
 
 
@@ -96,11 +173,42 @@ public class IniciarRecorridoActivity extends FragmentActivity implements OnMapR
 
     protected LocationRequest createLocationRequest() {
         LocationRequest mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(10000); //tasa de refresco en milisegundos
-        mLocationRequest.setFastestInterval(5000); //máxima tasa de refresco
+        mLocationRequest.setInterval(20000); //cada 20 seg se obtiene la ubicación del usuario
+        mLocationRequest.setFastestInterval(20000); //máxima tasa de refresco
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         return mLocationRequest;
     }
+
+    private void colocarMarcadorInicio (Location location){
+        LatLng inicio = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(inicio).title("Inicio"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(inicio));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(10));
+        latitudUltimaUbicacion = inicio.latitude;
+        longitudUltimaUbicacion = inicio.longitude;
+    }
+
+    private void actualizarUbicacion (Location location){
+        dibujarRuta (location);
+        calcularCampos (location);
+        latitudUltimaUbicacion = location.getLatitude();
+        longitudUltimaUbicacion = location.getLongitude();
+    }
+
+    private void dibujarRuta (Location location){
+        PolylineOptions options = new PolylineOptions().width(5).color(Color.BLUE).geodesic(true);
+        options.add(new LatLng(latitudUltimaUbicacion, longitudUltimaUbicacion));
+        options.add(new LatLng(location.getLatitude(), location.getLongitude()));
+        mMap.addPolyline(options);
+    }
+
+    private void calcularCampos (Location location){
+        //DISTANCIA
+        distanciaRecorridoN += UtilsJhonny.distance (location.getLatitude(), location.getLongitude(), latitudUltimaUbicacion, longitudUltimaUbicacion);
+        distanciaRecorrido.setText("Distancia: "+distanciaRecorridoN+" km");
+    }
+
+
 
 
 
