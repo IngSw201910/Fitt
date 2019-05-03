f34cf47892bd2f7f20d83fd4f8c711da38f35ef3
Miguel Angel Chaves <miguelangelipn@outlook.com>
2019-05-03 02:56:07 -0500

    Enlazamiento en Activities entrenador
    Se enlazar Mientrenador con mainEntrenador

--- Fitt/app/src/main/java/co/edu/javeriana/bittus/fitt/Vista/EntrenadorActivity.java
+++ Fitt/app/src/main/java/co/edu/javeriana/bittus/fitt/Vista/EntrenadorActivity.java
@@ -1,7 +1,9 @@
 package co.edu.javeriana.bittus.fitt.Vista;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import co.edu.javeriana.bittus.fitt.R;
 
@@ -11,6 +13,11 @@ public class EntrenadorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entrenador);
+    }
+
+    public void goToMiEntrenador(View view){
+        Intent nextActivity = new Intent(this, Entrenador_miEntrenadorActivity.class);
+        startActivity(nextActivity);
     }
 
     //Prueba Commit
