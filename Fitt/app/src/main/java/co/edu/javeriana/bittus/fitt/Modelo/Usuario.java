466a6edabc93758c0d39a280ff94c8f28a322dfe
Miguel Angel <miguelito_goz360@hotmail.com>
2019-05-03 23:28:33 -0500

    se agregaron los demas atributos del usuario y su constructor

--- Fitt/app/src/main/java/co/edu/javeriana/bittus/fitt/Modelo/Usuario.java
+++ Fitt/app/src/main/java/co/edu/javeriana/bittus/fitt/Modelo/Usuario.java
@@ -1,14 +1,97 @@
 package co.edu.javeriana.bittus.fitt.Modelo;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Usuario {
 
     private List<Rutina> rutinaList;
+    private String nombre;
+    private String correo;
+    private String contraseña;
+    private String direccionFoto;
+    private Date fechaNacimiento;
+    private String sexo;
+    private float altura;
+    private float peso;
 
 
-    public Usuario() {
 
+    public Usuario(String nombre, String correo, String contraseña, String direccionFoto, Date fechaNacimiento, String sexo, float altura, float peso) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.contraseña = contraseña;
+        this.direccionFoto = direccionFoto;
+        this.fechaNacimiento = fechaNacimiento;
+        this.sexo = sexo;
+        this.altura = altura;
+        this.peso = peso;
+        rutinaList = new ArrayList<Rutina>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getDireccionFoto() {
+        return direccionFoto;
+    }
+
+    public void setDireccionFoto(String direccionFoto) {
+        this.direccionFoto = direccionFoto;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
     }
 
     public List<Rutina> getRutinaList() {
