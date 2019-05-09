fe6678428c28a8539aaf07382b9ddb0db3c125fd
Jhonny <jhonny_parra@javeriana.edu.co>
2019-05-08 20:19:42 -0500

    Fragment perfil agregado al panel de navegación

--- Fitt/app/src/main/java/co/edu/javeriana/bittus/fitt/Vista/PopUps/PerfilFragment.java
+++ Fitt/app/src/main/java/co/edu/javeriana/bittus/fitt/Vista/PopUps/PerfilFragment.java
@@ -30,64 +30,13 @@ public class PerfilFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_comunidad, null, false);
+        return inflater.inflate(R.layout.fragment_perfil, null, false);
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         View v = getView();
-
-        buscarRutinasB = v.findViewById(R.id.buttonBuscarRutinas);
-
-
-        parquesB = v.findViewById(R.id.buttonParques);
-        chatsB = v.findViewById(R.id.buttonChats);
-        mapaUsuariosB = v.findViewById(R.id.buttonMapaUsuarios);
-        buscarUsuarioB = v.findViewById(R.id.buttonBuscarUsuario);
-        ejerciciosB = v.findViewById(R.id.buttonEjercicios);
-
-        parquesB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getActivity(), ParquesActivity.class));
-            }
-        });
-
-        buscarRutinasB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getActivity(), BuscarRutinas.class));
-            }
-        });
-
-        chatsB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getActivity(), ChatsActivity.class));
-            }
-        });
-
-        mapaUsuariosB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getActivity(), MapaUsuariosActivity.class));
-            }
-        });
-
-        buscarUsuarioB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getActivity(), BuscarUsuarioActivity.class));
-            }
-        });
-
-        ejerciciosB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getActivity(), EjerciciosActivity.class));
-            }
-        });
 
     }
 }
