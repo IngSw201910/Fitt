7344346b19334b96b98767fcae20d86df6d050e9
Laura-Mora <46944834+Laura-Mora@users.noreply.github.com>
2019-05-08 19:44:34 -0500

    Versión 1  perfil usuario
    Creación Fragment perfil usuario

---
+++ Fitt/app/src/main/java/co/edu/javeriana/bittus/fitt/Vista/PopUps/PerfilFragment.java
@@ -0,0 +1,93 @@
+package co.edu.javeriana.bittus.fitt.Vista.PopUps;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import co.edu.javeriana.bittus.fitt.R;
+import co.edu.javeriana.bittus.fitt.Vista.BuscarRutinas;
+import co.edu.javeriana.bittus.fitt.Vista.BuscarUsuarioActivity;
+import co.edu.javeriana.bittus.fitt.Vista.ChatsActivity;
+import co.edu.javeriana.bittus.fitt.Vista.EjerciciosActivity;
+import co.edu.javeriana.bittus.fitt.Vista.MapaUsuariosActivity;
+import co.edu.javeriana.bittus.fitt.Vista.ParquesActivity;
+
+public class PerfilFragment extends Fragment {
+
+    ImageButton buscarRutinasB;
+    ImageButton parquesB;
+    ImageButton chatsB;
+    ImageButton mapaUsuariosB;
+    ImageButton buscarUsuarioB;
+    ImageButton ejerciciosB;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_comunidad, null, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        View v = getView();
+
+        buscarRutinasB = v.findViewById(R.id.buttonBuscarRutinas);
+
+
+        parquesB = v.findViewById(R.id.buttonParques);
+        chatsB = v.findViewById(R.id.buttonChats);
+        mapaUsuariosB = v.findViewById(R.id.buttonMapaUsuarios);
+        buscarUsuarioB = v.findViewById(R.id.buttonBuscarUsuario);
+        ejerciciosB = v.findViewById(R.id.buttonEjercicios);
+
+        parquesB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), ParquesActivity.class));
+            }
+        });
+
+        buscarRutinasB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), BuscarRutinas.class));
+            }
+        });
+
+        chatsB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), ChatsActivity.class));
+            }
+        });
+
+        mapaUsuariosB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), MapaUsuariosActivity.class));
+            }
+        });
+
+        buscarUsuarioB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), BuscarUsuarioActivity.class));
+            }
+        });
+
+        ejerciciosB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), EjerciciosActivity.class));
+            }
+        });
+
+    }
+}
