4f1d12daa9e3f08f6d3e8f59ab843af772b4daed
Jhonny <jhonny_parra@javeriana.edu.co>
2019-05-09 20:00:30 -0500

    Usuario como en develop

--- Fitt/app/src/main/java/co/edu/javeriana/bittus/fitt/Modelo/Usuario.java
+++ Fitt/app/src/main/java/co/edu/javeriana/bittus/fitt/Modelo/Usuario.java
@@ -1,25 +1,105 @@
 package co.edu.javeriana.bittus.fitt.Modelo;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Usuario {
 
-    private int ID;
+    private List<Entrenamiento> entrenamientoList;
     private String nombre;
+    private String correo;
+    private String contraseña;
+    private String direccionFoto;
+    private Date fechaNacimiento;
+    private String sexo;
+    private float altura;
+    private float peso;
 
-    private List<Rutina> rutinaList;
 
 
-    public Usuario() {
-
+    public Usuario(String nombre, String correo, String contraseña, String direccionFoto, Date fechaNacimiento, String sexo, float altura, float peso) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.contraseña = contraseña;
+        this.direccionFoto = direccionFoto;
+        this.fechaNacimiento = fechaNacimiento;
+        this.sexo = sexo;
+        this.altura = altura;
+        this.peso = peso;
+        entrenamientoList = new ArrayList<Entrenamiento>();
     }
 
-    public List<Rutina> getRutinaList() {
-        return rutinaList;
+    public String getNombre() {
+        return nombre;
     }
 
-    public void setRutinaList(List<Rutina> rutinaList) {
-        this.rutinaList = rutinaList;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getDireccionFoto() {
+        return direccionFoto;
+    }
+
+    public void setDireccionFoto(String direccionFoto) {
+        this.direccionFoto = direccionFoto;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public List<Entrenamiento> getEntrenamientoList() {
+        return entrenamientoList;
+    }
+
+    public void setEntrenamientoList(List<Entrenamiento> entrenamientoList) {
+        this.entrenamientoList = entrenamientoList;
     }
 
     //usuario cambiado
