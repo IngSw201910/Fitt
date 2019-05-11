a103be9a15335694c54c4257fd8562121f1a25f3
Jhonny <jhonny_parra@javeriana.edu.co>
2019-05-11 03:07:30 -0500

    Primera versión de realizar entrenamiento
    Falta conectar con Firebase
    Falta pausar y reanudar
    Falta cambiar el idioma a español y ver si funciona en otro celular
    Falta dar información al inicio de cada serie

--- Fitt/app/src/main/java/co/edu/javeriana/bittus/fitt/Vista/EjercicioRepeticionesFragment.java
+++ Fitt/app/src/main/java/co/edu/javeriana/bittus/fitt/Vista/EjercicioRepeticionesFragment.java
@@ -1,36 +1,72 @@
 package co.edu.javeriana.bittus.fitt.Vista;
 
-import android.content.Intent;
+import android.app.Activity;
+import android.content.Context;
+import android.os.Handler;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-import com.google.firebase.auth.FirebaseAuth;
+import java.util.concurrent.TimeUnit;
 
+import co.edu.javeriana.bittus.fitt.Modelo.EjercicioRepeticiones;
+import co.edu.javeriana.bittus.fitt.Modelo.Ejercicio;
 import co.edu.javeriana.bittus.fitt.R;
-import co.edu.javeriana.bittus.fitt.Utilidades.Utils;
 
 public class EjercicioRepeticionesFragment extends Fragment {
+    private FragmentEjercicioRepeticionesListener listener;
+
+    private TextView descripcion;
+
+    private TextView series;
+    private TextView repeticiones;
 
 
+    private ProgressBar seriesPB;
+    private ProgressBar repeticionesPB;
 
 
+    private EjercicioRepeticiones ejercicioRepeticiones;
+    private int serie;
+    private int repeticion = 0;
 
 
+    private TextToSpeech textToSpeech;
 
+    private int estado;
+
+    private static final int CORRIENDO = 0;
+    private static final int PAUSADO = 1;
+
+    private boolean ejercicioTerminado = false;
+
+    Thread manejadorEjercicio;
+
+    Activity realizarEntrenaActivity;
+
+    public interface FragmentEjercicioRepeticionesListener {
+        void mostrarSiguienteEjercicio();
+
+        void darInstrucciones(String texto);
+
+    }
 
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ejercicioRepeticiones = (EjercicioRepeticiones) getArguments().getSerializable("ejercicioRepeticiones");
+        serie = getArguments().getInt("serie");
         return inflater.inflate(R.layout.fragment_ejercicio_repeticiones, null, false);
     }
-
 
 
     @Override
@@ -38,9 +74,98 @@ public class EjercicioRepeticionesFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
 
         View v = getView();
+        TextView descripcion = (TextView) v.findViewById(R.id.descripcionEjercicioRepeticiones);
+        descripcion.setMovementMethod(new ScrollingMovementMethod());
+
+        series = v.findViewById(R.id.tvSerieRepeticiones);
+        repeticiones = v.findViewById(R.id.tvRepeticiones);
 
 
+        seriesPB = v.findViewById(R.id.pbSerieRepeticiones);
+        repeticionesPB = v.findViewById(R.id.pbRepeticiones);
+
+
+        seriesPB.setMax(ejercicioRepeticiones.getSeries());
+        seriesPB.setProgress(serie);
+        series.setText(serie + "/" + ejercicioRepeticiones.getSeries());
+
+
+        repeticionesPB.setMax(ejercicioRepeticiones.getRepeticiones());
+        repeticionesPB.setProgress(0);
+        repeticiones.setText(repeticion + "/" + ejercicioRepeticiones.getRepeticiones());
+
+        iniciarEjercicio();
 
 
     }
+
+    private void iniciarEjercicio() {
+
+        manejadorEjercicio = new Thread() {
+            public void run() {
+                while (repeticion < ejercicioRepeticiones.getRepeticiones()) {
+                    if (estado == CORRIENDO) {
+                        listener.darInstrucciones("1");
+                        try {
+                            sleep(1000);
+                        } catch (InterruptedException e) {
+                            this.interrupt();
+                            break;
+                        }
+                        listener.darInstrucciones("2");
+                        repeticion++;
+                        getActivity().runOnUiThread(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                repeticionesPB.setProgress(repeticion);
+                                repeticiones.setText(repeticion + "/" + ejercicioRepeticiones.getRepeticiones());
+                            }
+                        });
+
+
+                        try {
+                            sleep(1000);
+                        } catch (InterruptedException e) {
+                            this.interrupt();
+                            break;
+                        }
+
+                    }
+
+                }
+                if (!this.isInterrupted()) {
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            listener.mostrarSiguienteEjercicio();
+                        }
+                    });
+                }
+            }
+        };
+
+        manejadorEjercicio.start();
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof FragmentEjercicioRepeticionesListener) {
+            listener = (FragmentEjercicioRepeticionesListener) context;
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        if (manejadorEjercicio != null)
+            manejadorEjercicio.interrupt();
+        listener = null;
+        super.onDetach();
+
+
+    }
+
+
 }
