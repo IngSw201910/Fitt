6c8f72bc3bd2c46e079b859610c338d0d247f460
Miguel Angel Chaves <miguelangelipn@outlook.com>
2019-05-13 13:35:47 -0500

    Mostrar usuarios en el chat
    Primera version, falta desplegar a los usuarios en el RecyclerView

--- Fitt/app/src/main/java/co/edu/javeriana/bittus/fitt/Vista/ChatsActivity.java
+++ Fitt/app/src/main/java/co/edu/javeriana/bittus/fitt/Vista/ChatsActivity.java
@@ -2,6 +2,8 @@ package co.edu.javeriana.bittus.fitt.Vista;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 import co.edu.javeriana.bittus.fitt.R;
 
@@ -11,5 +13,10 @@ public class ChatsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chats);
+    }
+
+    public void gotoNewMessage(View view){
+        Intent nextActivity = new Intent(this, nuevoMensajeActivity.class);
+        startActivity(nextActivity);
     }
 }
