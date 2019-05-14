258ef766fc9405c4c5d78d62fa98a36e73525b7c
Miguel Angel Chaves <miguelangelipn@outlook.com>
2019-05-14 04:08:25 -0500

    Envio de Mensajes a Firebase
    El Chat ya manda mensajes a la base de datos, falta desplegarlos

--- Fitt/app/src/main/java/co/edu/javeriana/bittus/fitt/Vista/MensajeActivity.java
+++ Fitt/app/src/main/java/co/edu/javeriana/bittus/fitt/Vista/MensajeActivity.java
@@ -1,11 +1,16 @@
 package co.edu.javeriana.bittus.fitt.Vista;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -14,6 +19,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 import co.edu.javeriana.bittus.fitt.Modelo.Usuario;
 import co.edu.javeriana.bittus.fitt.R;
@@ -27,6 +34,9 @@ public class MensajeActivity extends AppCompatActivity {
     FirebaseUser firebaseUser;
     DatabaseReference databaseReference;
 
+    ImageButton btn_sendMessage;
+    EditText text_send;
+
     Intent intent;
 
     @Override
@@ -36,9 +46,11 @@ public class MensajeActivity extends AppCompatActivity {
 
         nombre_usuario_msg = findViewById(R.id.txt_msg_username);
         profile_image_msg = findViewById(R.id.img_msg_foto);
+        btn_sendMessage = findViewById(R.id.btn_sendMessage);
+        text_send = findViewById(R.id.text_send);
 
         intent = getIntent();
-        String userid = intent.getStringExtra("id");
+        final String userid = intent.getStringExtra("id");
 
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         databaseReference = FirebaseDatabase.getInstance().getReference("Usuarios").child(userid);
@@ -56,5 +68,30 @@ public class MensajeActivity extends AppCompatActivity {
 
             }
         });
+
+        btn_sendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = text_send.getText().toString();
+                if(!msg.equals("")){
+                    sendMessage(firebaseUser.getUid(), userid, msg);
+                }else{
+                    Toast.makeText(MensajeActivity.this, "No ha escrito nada", Toast.LENGTH_SHORT).show();
+                }
+                text_send.setText("");
+            }
+        });
+
+    }
+
+    private void sendMessage(String sender, String receiver, String message){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("sender", sender);
+        hashMap.put("receiver", receiver);
+        hashMap.put("message", message);
+
+        reference.child("Chats").push().setValue(hashMap);
     }
 }
