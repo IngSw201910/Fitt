90222afaafb484242454208a596569b325c17d29
Miguel Angel Chaves <miguelangelipn@outlook.com>
2019-05-22 03:43:09 -0500

    Implementacion popUps Entrenador
    Se agregaron popUps a entrenador con el fin de alertar al usuario que no tiene entrenador asignado.

--- Fitt/app/src/main/java/co/edu/javeriana/bittus/fitt/Vista/EntrenadorActivity.java
+++ Fitt/app/src/main/java/co/edu/javeriana/bittus/fitt/Vista/EntrenadorActivity.java
@@ -9,27 +9,47 @@ import android.view.View;
 
 import co.edu.javeriana.bittus.fitt.R;
 import co.edu.javeriana.bittus.fitt.Modelo.Entrenador;
-
+import co.edu.javeriana.bittus.fitt.Vista.PopUps.PopSolicitarEntrenador;
 
 
 public class EntrenadorActivity extends AppCompatActivity {
 
-    //Entrenador coach = new Entrenador("Camilo Perez");
+    String idEntrenador = "default";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entrenador);
+
+        //Checar si tiene el cliente un entrenador asociado;
+    }
+
+    public boolean tieneEntrenador(String idEntrenador){
+        if(idEntrenador.equals("default")){
+            return false;
+        }else{
+            return true;
+        }
     }
 
     public void goToMiEntrenador(View view){
-        Intent nextActivity = new Intent(this, Entrenador_miEntrenadorActivity.class);
-        //nextActivity.putExtra("entrenador", coach);
-        //startActivity(nextActivity);
+        if(tieneEntrenador(idEntrenador)){
+            Intent nextActivity = new Intent(this, Entrenador_miEntrenadorActivity.class);
+            //nextActivity.putExtra("entrenador", coach);
+            //startActivity(nextActivity);
+        }else{
+            Intent nextPopUp = new Intent(this, PopSolicitarEntrenador.class);
+            startActivity(nextPopUp);
+        }
     }
     public void goToChatEntrenador(View view){
-        Intent nextActivity = new Intent(this, ChatsActivity.class);
-        startActivity(nextActivity);
+        if(tieneEntrenador(idEntrenador)){
+            Intent nextActivity = new Intent(this, ChatsActivity.class);
+            startActivity(nextActivity);
+        }else{
+            Intent nextPopUp = new Intent(this, PopSolicitarEntrenador.class);
+            startActivity(nextPopUp);
+        }
     }
 
     public void goToBuscarEntrenador(View view){
@@ -37,3 +57,5 @@ public class EntrenadorActivity extends AppCompatActivity {
         startActivity(nextActivity);
     }
 }
+
+
---
+++ Fitt/app/src/main/java/co/edu/javeriana/bittus/fitt/Vista/PopUps/PopSolicitarEntrenador.java
@@ -0,0 +1,23 @@
+package co.edu.javeriana.bittus.fitt.Vista.PopUps;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+import co.edu.javeriana.bittus.fitt.R;
+
+public class PopSolicitarEntrenador extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pop_solicitarentrenador);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        int widthDisplay = displayMetrics.widthPixels;
+        int heightDisplay = displayMetrics.heightPixels;
+
+        getWindow().setLayout((int) (widthDisplay*0.8),(int) (heightDisplay*0.5));
+    }
+}
