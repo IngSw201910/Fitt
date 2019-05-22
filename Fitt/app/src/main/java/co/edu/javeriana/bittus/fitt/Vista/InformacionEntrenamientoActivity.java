00c63812df11a7b4b6e331aa022b8c8da5ebd687
Miguel Angel <miguelito_goz360@hotmail.com>
2019-05-22 14:49:01 -0500

    mini commit

--- Fitt/app/src/main/java/co/edu/javeriana/bittus/fitt/Vista/InformacionEntrenamientoActivity.java
+++ Fitt/app/src/main/java/co/edu/javeriana/bittus/fitt/Vista/InformacionEntrenamientoActivity.java
@@ -11,7 +11,6 @@ import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
