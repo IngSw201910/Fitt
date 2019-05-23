fa4a2b17707d1cc032f5e695db5f0437b0fcba4e
Laura-Mora <46944834+Laura-Mora@users.noreply.github.com>
2019-05-23 00:52:28 -0500

    Mostrar Usuario

--- Fitt/app/src/main/java/co/edu/javeriana/bittus/fitt/Vista/MostrarUsuarioActivity.java
+++ Fitt/app/src/main/java/co/edu/javeriana/bittus/fitt/Vista/MostrarUsuarioActivity.java
@@ -3,18 +3,31 @@ package co.edu.javeriana.bittus.fitt.Vista;
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 
 import android.view.View;
 
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.io.Serializable;
 
 import co.edu.javeriana.bittus.fitt.Modelo.Usuario;
 
 import co.edu.javeriana.bittus.fitt.R;
+import co.edu.javeriana.bittus.fitt.Utilidades.RutasBaseDeDatos;
+
+import static co.edu.javeriana.bittus.fitt.Utilidades.PersistenciaFirebase.descargarFotoYPonerEnImageView;
 
 
 //Mostrar Usuario que no se sigue
@@ -22,6 +35,7 @@ public class MostrarUsuarioActivity extends AppCompatActivity {
 
 
     private Usuario item;
+    private Usuario usuarioConectado;
 
     private TextView nombre;
     private Button seguidos;
@@ -30,12 +44,37 @@ public class MostrarUsuarioActivity extends AppCompatActivity {
     private Button mensaje;
     private Button ubicacion;
     private TextView entrenador;
+    private ImageView fotoU;
+
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+    DatabaseReference myRef2;
+    private FirebaseUser mAuth;
+
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mostrar_usuario);
 
         item= (Usuario) getIntent().getSerializableExtra("objectData");
+
+        mAuth = FirebaseAuth.getInstance().getCurrentUser();
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference(RutasBaseDeDatos.RUTA_USUARIOS).child(mAuth.getUid());
+
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                usuarioConectado = dataSnapshot.getValue(Usuario.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         nombre = findViewById(R.id.textViewNombreUsuarioN);
         nombre.setText(item.getNombre());
@@ -46,10 +85,19 @@ public class MostrarUsuarioActivity extends AppCompatActivity {
         seguidores =findViewById(R.id.buttonSeguidoresUN);
         seguidores.setText(item.getSeguidoresList().size()+" seguidores");
 
+        fotoU= findViewById(R.id.imageViewFotoUsuarioN);
+        descargarFotoYPonerEnImageView(item.getDireccionFoto(),fotoU);
+
         seguir =findViewById(R.id.buttonSeguirUN);
         seguir.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+
+                usuarioConectado.getSeguidosList().add(item);
+                myRef.setValue(usuarioConectado);
+                item.getSeguidoresList().add(usuarioConectado);
+                myRef2 = database.getReference(RutasBaseDeDatos.RUTA_USUARIOS).child(item.getId());
+                myRef2.setValue(item);
 
 
             }
